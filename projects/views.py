504e41d5f9606e8a19a281e99931812e47332e9d
Dayana <dayana.nashkova@yahoo.com>
2023-06-24 11:10:04 +0000

    query the database and display projects

--- projects/views.py
+++ projects/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 # Create your views here.
 
 projectsList = [
 
-{'id': '1', 'title': 'Ecommerce Website', 'description': 'Fully functional ecommerce website' },
+    {'id': '1', 'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'},
 
-{ 'id': '2', 'title': 'Portfolio Website', 'description': 'A personal website to write articles and display work' },
+    {'id': '2', 'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'},
 
-{'id': '3', 'title': 'Social Network', 'description': 'An open source project built by the community' }
+    {'id': '3', 'title': 'Social Network',
+        'description': 'An open source project built by the community'}
 
 ]
 
+
 def projects(request):
-    page = 'projects'
-    number = 8
+    projects = Project.objects.all()
     context = {
-        'page':page,
-        'number': number
+        'projects': projects
     }
     return render(request, 'projects/projects.html', context)
 
+
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-    return render(request, 'projects/single-project.html',{'project': projectObj})
+    projectObj = Project.objects.get(id=pk)
+
+    return render(request, 'projects/single-project.html', {'project': projectObj})
