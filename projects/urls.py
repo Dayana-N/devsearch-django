3dc68041068e432070811a6066c16f87f296d16f
Dayana <dayana.nashkova@yahoo.com>
2023-06-24 11:42:37 +0000

    feat: add create project function

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 
     path('project/<str:pk>/', views.project, name='project'),
     path('', views.projects, name='projects'),
+    path('create-project/', views.createProject, name='create-project'),
 ]
