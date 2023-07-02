1265db84c33bf6908fc3ab8dfccb8692f6d7947c
Dayana <dayana.nashkova@yahoo.com>
2023-07-02 18:31:35 +0000

    add user edit page

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 from django.views.decorators.csrf import requires_csrf_token
 
 # Create your views here.
@@ -55,7 +55,7 @@ def registerUser(request):
 
             messages.success(request, 'User account was created!')
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
         else:
             messages.error(request, 'An Error has occurred')
 
@@ -98,3 +98,17 @@ def userAccount(request):
         'projects': projects
     }
     return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
